f9d45dfbf5113216aacb448fd5495b9d24413afc
Jade <jade@pc-de-jade.home>
2023-05-17 11:31:35 +0200

    refactor(test): adapt the unit tests

--- test/filter.test.js
+++ test/filter.test.js
@@ -1,9 +1,11 @@
-const {filter} = require('../app');
+const { filter } = require("../app");
 
-describe('filter test', () => {
-    it('should show the animals matching with the ry string pattern', () => {
-        const result = filter('ry');
+describe("filter test", () => {
+  it("should show the animals matching with the ry string pattern", () => {
+    const result = filter("ry");
 
-        expect(result).toEqual('[{"name":"Uzuzozne","people":[{"name":"Lillie Abbott","animals":[{"name":"John Dory"}]}]},{"name":"Satanwi","people":[{"name":"Anthony Bruno","animals":[{"name":"Oryx"}]}]}]');
-    });
+    expect(JSON.stringify(result)).toEqual(
+      '[{"name":"Uzuzozne","people":[{"name":"Lillie Abbott","animals":[{"name":"John Dory"}]}]},{"name":"Satanwi","people":[{"name":"Anthony Bruno","animals":[{"name":"Oryx"}]}]}]'
+    );
+  });
 });
